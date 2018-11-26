a08a51ab25763731ae364aab8335aa1ecf8a0ce0
Paolo Chidrome <chidrome@Paolos-MacBook-Pro.local>
2018-11-25 21:29:32 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,6 @@
+var cardOne = "Queen of Diamonds";
+var cardTwo = "Queen of Hearts";
+var cardThree = "King of Diamonds";
+var cardFour = "King of Hearts";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
