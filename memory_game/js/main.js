8a0bd0634c73ab31b6fed5fa459545d2231f93bf
Paolo Chidrome <chidrome@Paolos-MacBook-Pro.local>
2018-11-26 22:33:42 -0800

    changed repository loc. Added main.js file

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,18 @@
-var cardOne = "Queen of Diamonds";
-var cardTwo = "Queen of Hearts";
-var cardThree = "King of Diamonds";
-var cardFour = "King of Hearts";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = []
+var cardOne = cards[0]
+var cardTwo = cards[2]
+cardsInPlay.push(cardOne)
+cardsInPlay.push(cardTwo)
+console.log("User flipped " + cardOne)
+console.log("User flipped " + cardTwo)
+
+if (cardsInPlay.length == 2) {
+	if (cardOne == cardTwo) {
+		alert("You've found a match");
+	} else {
+		alert("Sorry, try again.");
+	}
+} else {
+	alert("Please chose a second card")
+}
